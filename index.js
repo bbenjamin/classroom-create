186150e6868d614b7b83dfbd6cac886c3e0235dc
Mullins, Benjamin <benm@umich.edu>
2023-12-28 19:40:57 -0500

    builded

--- index.js
+++ index.js
@@ -4,19 +4,21 @@ const { Toolkit } = require('actions-toolkit');
 
 const tools = new Toolkit();
 
+const admins = ['bbenjamin', 'XinranCao']
+
 try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
   console.log(`Hello ${nameToGreet}!`);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
-  const permissionPayload =  tools.github.repos.getCollaboratorPermissionLevel({
-    ...tools.context.repo,
-    username: tools.context.actor
-  });
+  // const permissionPayload =  tools.github.repos.getCollaboratorPermissionLevel({
+  //   ...tools.context.repo,
+  //   username: tools.context.actor
+  // });
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify({...github.context.payload, hoobastank: permissionPayload}, undefined, 2)
-  console.log(`The event payload?: ${payload}`);
+  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  console.log(`Theee event payload?: ${payload}`);
 
 
 } catch (error) {
