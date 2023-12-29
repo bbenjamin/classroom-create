193627e2bebfe3116a7bb3e6be1457a1f164db14
Mullins, Benjamin <benm@umich.edu>
2023-12-29 15:37:59 -0500

    uhg

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 const { Toolkit } = require('actions-toolkit');
+const { exec } = require('node:child_process')
+
 
 const tools = new Toolkit();
 
@@ -17,7 +19,7 @@ try {
   const soft_fail = core.getInput("soft_fail")
 
   const client = github.getOctokit(token)
-  console.log(`THE CLIE ELI ${client.rest.users.getAuthenticated()}`);
+  // console.log(`THE CLIE ELI ${client.rest.users.getAuthenticated()}`);
 
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
@@ -32,6 +34,16 @@ try {
 
   if (!isAdmin) {
     console.info('THIS IS NOT AN ADMIN AND THUS NEEDS RESTRICTING')
+    exec('git branch -D solution', (err, output) => {
+      // once the command has completed, the callback function is called
+      if (err) {
+        // log and return if we encounter an error
+        console.error("could not execute command: ", err)
+        return
+      }
+      // log the output received from the command
+      console.log("Output: \n", output)
+    }))
   } else {
     console.log('THIS WAS SEEN AS ADMIN, IT LETS THING HAPPEN.')
   }
@@ -41,7 +53,7 @@ try {
   // });
   // Get the JSON webhook payload for the event that triggered the workflow
   // const payload = JSON.stringify(github, undefined, 2)
-  const payload = JSON.stringify(Object.keys(client), undefined, 2)
+  const payload = JSON.stringify(Object.keys(client.rest), undefined, 2)
   console.log(`The entire github context forrrr the varia les: ${payload}`);
 
 
