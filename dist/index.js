5261915320fd731a106eb38bcdf146b27fbab98c
Mullins, Benjamin <benm@umich.edu>
2023-12-30 17:15:36 -0500

    add aditya

--- dist/index.js
+++ dist/index.js
@@ -47985,24 +47985,12 @@ const { Toolkit } = __nccwpck_require__(762);
 
 const tools = new Toolkit();
 
-const admins = ['bbenjamin', 'XinranCao']
+const admins = ['bbenjamin', 'XinranCao', 'supraditya']
 
 try {
   const token = core.getInput("github_token", { required: true })
-  const numbers = core.getInput("numbers")
-  const owner = core.getInput("owner")
-  const repository = core.getInput("repository")
-  const branches = core.getInput("branches")
-  const prefix = core.getInput("prefix")
-  const suffix = core.getInput("suffix")
-  const soft_fail = core.getInput("soft_fail")
-
   const client = github.getOctokit(token)
-  // console.log(`THE CLIE ELI ${client.rest.users.getAuthenticated()}`);
 
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
 
@@ -48016,11 +48004,8 @@ try {
     console.info('THIS IS NOT AN ADMIN AND THUS NEEDS RESTRICTING')
 
   } else {
-    console.log('THIS WAS SEEN AS ADMIN, IT LETS THING HAPPEN.')
+    console.log('THIS WAS SEEN AS ADMIN, DO NOT PRUNE ANYTHING.')
   }
-
-  const payload = JSON.stringify(github, undefined, 2)
-  console.log(`big  bad output: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
 }
