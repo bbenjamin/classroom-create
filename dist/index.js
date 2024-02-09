9b88070d755c02d145328241e71c7ee162aea699
Mullins, Benjamin <benm@umich.edu>
2024-02-09 12:24:27 -0500

    see if not using some() helps

--- dist/index.js
+++ dist/index.js
@@ -47994,11 +47994,16 @@ try {
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   console.info(`Check against ${github.context.payload.repository['full_name']}`);
+  let adminCheck = false;
   const isAdmin = admins.some(name => {
-      const regex = new RegExp(`\-${name}$`, 'g');
-      console.info(`Does ${name} do it? ${regex.test(github.context.payload.repository['full_name'])}`)
-      return regex.test(github.context.payload.repository['full_name']);
+    const regex = new RegExp(`\-${name}$`, 'g');
+    console.info(`Does ${name} do it? ${regex.test(github.context.payload.repository['full_name'])}`)
+    if (!adminCheck) {
+      adminCheck = regex.test(github.context.payload.repository['full_name']);
+    }
+    return regex.test(github.context.payload.repository['full_name']);
   })
+  console.info(`isAdmin: ${isAdmin} | adminCheck: ${adminCheck}`);
   core.setOutput("is-student", (!isAdmin && !github.context.payload.repository.is_template) ? '1' : '0');
 
   if (!isAdmin) {
